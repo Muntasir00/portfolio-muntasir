b92c5eac19eeb35d9660182bc2fd7dcf72a01cc8
muntasir-00 <muntasir@reeadigital.com>
2025-08-14 23:27:05 +0600

    work import renamed

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import Header from './components/Header';
 import About from './components/About';
 import Services from './components/Services';
-import Work from './components/Work';
+import Work from './components/work';
 import Contact from './components/Contact';
 
 export default function Home() {
